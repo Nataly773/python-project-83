a1a41e934ffcb1917ac3b18c82d4364ad9c9266c
Нечаева Наталья <miss.nechaewa@gmail.com>
2025-07-30 22:33:14 +0300

    add .

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from dotenv import load_dotenv
 from urllib.parse import urlparse
 import validators
+from flask import abort
 
 load_dotenv()
 
@@ -51,7 +52,13 @@ def add_url():
 def list_urls():
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT id, name, created_at FROM urls ORDER BY id DESC")
+            cur.execute("""
+                SELECT urls.id, urls.name, urls.created_at, MAX(url_checks.created_at) AS last_check
+                FROM urls
+                LEFT JOIN url_checks ON urls.id = url_checks.url_id
+                GROUP BY urls.id
+                ORDER BY urls.id DESC
+            """)
             urls = cur.fetchall()
     return render_template('urls/index.html', urls=urls)
 
@@ -62,4 +69,31 @@ def show_url(id):
         with conn.cursor() as cur:
             cur.execute("SELECT id, name, created_at FROM urls WHERE id = %s", (id,))
             url = cur.fetchone()
-    return render_template('urls/show.html', url=url)
+            if not url:
+                abort(404)
+
+            cur.execute(
+                "SELECT * FROM url_checks WHERE url_id = %s ORDER BY id DESC",
+                (id,)
+            )
+            checks = cur.fetchall()
+
+    return render_template('urls/show.html', url=url, checks=checks)
+
+
+@app.post('/urls/<int:id>/checks')
+def run_check(id):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT id FROM urls WHERE id = %s", (id,))
+            url = cur.fetchone()
+            if not url:
+                abort(404)
+
+            cur.execute(
+                "INSERT INTO url_checks (url_id, created_at) VALUES (%s, %s)",
+                (id, datetime.datetime.now())
+            )
+
+    flash('Проверка успешно добавлена', 'success')
+    return redirect(url_for('show_url', id=id))
